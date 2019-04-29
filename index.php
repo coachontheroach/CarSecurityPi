6f239ba0706c80818695fd04188a3d8147a14a56
Devin Suttles <devinfnsuttles@gmail.com>
2019-04-28 22:36:29 -0500

    Change the logic for starting a user session

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<? php
-  session_start();
-  if(!$_SESSION['auth'])
-  {
-    header("location:login.php");
-  }
-  else
-  {
-    include('resources/includes/headerMap.php');
-    include('resources/includes/footerMap.php');
-  }
+<?php
+session_start();
+if($_SESSION['auth'])
+{
+  header("location:login.php");
+}
+else
+{
+  include('resources/includes/headerMap.php');
+  include('resources/includes/footerMap.php');
+}
 ?>

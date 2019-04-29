a0af647723dadc7392362d85411d3f62696d3137
Devin Suttles <devinfnsuttles@gmail.com>
2019-04-28 22:34:59 -0500

    Remove the signup button

--- login.php
+++ login.php
@@ -1,37 +1,25 @@
 <?php
+include('resources/includes/header.php');
+include('resources/includes/footer.php');
+
 $dbhost = 'mysql.eecs.ku.edu';
 $dbuser = 'jkissick';
 $dbpass = 'AhVid7ie';
 $dbname = 'jkissick';
-$mysqli = new mysqli($dbhost, $dbuser,$dbpass,$dbname);
+$conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 
-/* check connection */
-if ($mysqli->connect_errno)
-{
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-$username=$_Post['username'];
-$password=$_Post['password'];
-$sql="SELECT * FROM CarSecurity WHERE user='$username' AND password='$password' LIMIT 1";
-
-$query = $mysqli->prepare($sql);
-
-
-$query->execute();
-
-  // Return row as an array indexed by both column name
-$returned_row = $query->fetch();
-
-if ($query->num_rows() > 0)
+$username=$_POST['username'];
+$password=$_POST['password'];
+$query="SELECT * FROM CarSecurity WHERE user='$username' AND password='$password'";
+$result=mysqli_query($conn,$query);
+if(mysqli_num_rows($result)==1)
 {
   session_start();
   $_SESSION['auth']='true';
   header('location:index.php');
 }
-?>
-
-<?php
-include('resources/includes/header.php');
-include('resources/includes/footer.php');
+else
+{
+  $_SESSION['auth']='false';
+}
 ?>

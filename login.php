78b6dc0b2c4e605c3590e4d63dbe7507624bfc02
Devin Suttles <devinfnsuttles@gmail.com>
2019-05-02 11:52:41 -0500

    Change the code to connect to jeff sql database

--- login.php
+++ login.php
@@ -1,25 +1,37 @@
 <?php
-include('resources/includes/header.php');
-include('resources/includes/footer.php');
-
 $dbhost = 'mysql.eecs.ku.edu';
 $dbuser = 'jkissick';
 $dbpass = 'AhVid7ie';
 $dbname = 'jkissick';
-$conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+/* check connection */
+if ($conn->connect_errno)
+{
+    printf("Connect failed: %s\n", $conn->connect_error);
+    exit();
+}
 
 $username=$_POST['username'];
 $password=$_POST['password'];
+
 $query="SELECT * FROM CarSecurity WHERE user='$username' AND password='$password'";
-$result=mysqli_query($conn,$query);
-if(mysqli_num_rows($result)==1)
+
+if($result = $conn->query($query))
 {
   session_start();
   $_SESSION['auth']='true';
   header('location:index.php');
+  $result->free();
 }
 else
 {
-  $_SESSION['auth']='false';
+  echo "Post was not saved because it was not written by an existing user.";
 }
+$conn->close();
+?>
+
+<?php
+include('resources/includes/header.php');
+include('resources/includes/footer.php');
 ?>
